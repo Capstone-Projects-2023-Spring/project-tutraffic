c59f4f4f3eb71350ad0fe84375c6fb89db75f42c
Raymond Chen <tuj76813@temple.edu>
2023-02-28 16:05:04 -0500

    Add feature to send message from frontend to backend

--- tutraffic-app/backend/app.py
+++ tutraffic-app/backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -8,7 +8,15 @@ CORS(app)
 def my_profile():
     response_body = {
         "name": "TuTraffic",
-        "about": "Hello! New way to find parking"
+        "message": "Hello! New way to find parking",
+        "space": 1
     }
 
     return response_body
+
+@app.route('/data', methods=['POST'])
+def receive_data():
+    received_data = request.get_json()
+    print(received_data)
+    response_body = {"message": "Data received successfully"}
+    return jsonify(response_body)
--- tutraffic-app/src/App.js
+++ tutraffic-app/src/App.js
@@ -4,37 +4,63 @@ import './App.css';
 
 function App() {
 
-   // store message
+  // store message
   const [profileData, setProfileData] = useState([]);
 
   // receive message
   function getData() {
     axios({
       method: "GET",
-      url:"http://127.0.0.1:5000/profile",
+      url: "http://127.0.0.1:5000/profile",
     })
-    .then((response) => {
-      const res =response.data
-      setProfileData(({
-        project: res.name,
-        about: res.about}))
-    }).catch((error) => {
-      if (error.response) {
-        console.log(error.response)
-        console.log(error.response.status)
-        console.log(error.response.headers)
+      .then((response) => {
+        const res = response.data
+        setProfileData(({
+          project: res.name,
+          message: res.message,
+          space: res.space
+        }))
+      }).catch((error) => {
+        if (error.response) {
+          console.log(error.response)
+          console.log(error.response.status)
+          console.log(error.response.headers)
         }
-    })}
+      })
+  }
+
+  function sendData() {
+    const data = {
+      name: "tutraffic",
+      email: "tutraffic@temple.edu"
+    };
+    axios({
+      method: "POST",
+      url: "http://127.0.0.1:5000/data",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      data: data
+    })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <p>Profile</p><button onClick={getData}>Get data</button>
         {profileData && <div>
-              <p>Project: {profileData.project}</p>
-              <p>About: {profileData.about}</p>
-            </div>
+          <p>Project: {profileData.project}</p>
+          <p>Message: {profileData.message}</p>
+          <p>Parking Space: {profileData.space}</p>
+        </div>
         }
+        <button onClick={sendData}>Send data</button>
       </header>
     </div>
   );
