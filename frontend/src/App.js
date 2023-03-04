c4e849106a916fa31a4d34b41f4bafcebbbf1290
Raymond Chen <97559049+Mo2artGit@users.noreply.github.com>
2023-03-04 19:44:21 +0000

    created nav bar and file structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,69 +1,22 @@
-import { useState } from 'react'
-import axios from "axios";
-import './App.css';
+import React from "react";
+import Navr from "./Navr";
+import Home from "./Home";
+import Data from "./Data";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
-
-  // store message
-  const [profileData, setProfileData] = useState([]);
-
-  // receive message
-  const getData = () => {
-    axios({
-      method: "GET",
-      url: "http://localhost:8000/profile",
-    })
-      .then((response) => {
-        const res = response.data
-        setProfileData(({
-          project: res.name,
-          message: res.message,
-          space: res.space
-        }))
-      }).catch((error) => {
-        if (error.response) {
-          console.log(error.response)
-          console.log(error.response.status)
-          console.log(error.response.headers)
-        }
-      })
-  };
-
-  const sendData = () => {
-    const data = {
-      name: "tutraffic",
-      email: "tutraffic@temple.edu"
-    };
-    axios({
-      method: "POST",
-      url: "http://localhost:8000/data",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      data: data
-    })
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.log(error.response);
-      });
-  };
-
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Profile</p><button onClick={getData}>Get data</button>
-        {profileData && <div>
-          <p>Project: {profileData.project}</p>
-          <p>Message: {profileData.message}</p>
-          <p>Parking Space: {profileData.space}</p>
-        </div>
-        }
-        <button onClick={sendData}>Send data</button>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navr />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/data" element={<Data />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+
---
+++ frontend/src/Home.js
@@ -0,0 +1,10 @@
+export default function Home() {
+
+    return (
+        <div>
+            <h1>Home page</h1>
+            <p> Google Map </p>
+        </div>
+    );
+
+}
