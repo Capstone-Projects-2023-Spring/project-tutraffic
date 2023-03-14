9868f734a72f2556970b279cb745532700d75c10
Raymond Chen <97559049+Mo2artGit@users.noreply.github.com>
2023-03-14 20:18:49 +0000

    display Map from google map api

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,10 +1,32 @@
+import React from 'react'
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+const containerStyle = {
+    width: '100vh',
+    height: '100vh'
+};
+
+const center = {
+    lat: 39.981,
+    lng: -75.155
+};
+
+
 export default function Home() {
 
     return (
-        <div>
-            <h1>Home page</h1>
-            <p> Google Map </p>
-        </div>
+        <LoadScript
+            googleMapsApiKey="EMPTY"
+        >
+            <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={center}
+                zoom={10}
+            >
+                { /* Child components, such as markers, info windows, etc. */}
+                <></>
+            </GoogleMap>
+        </LoadScript>
     );
 
 }
