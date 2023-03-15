f73b7325fe940996ab894f70eb0c285e5de4f293
Brian Rangel <brianrang13@gmail.com>
2023-03-14 20:36:29 -0400

    Delete accounts and modify account details in backend

--- backend/src/main/java/tutraffic/backend/BackendController.java
+++ backend/src/main/java/tutraffic/backend/BackendController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin
@@ -14,23 +15,53 @@ public class BackendController {
     @Autowired
     UserRepository userRepository;
 
+    // Create a user and store it in the database
     @PostMapping("/users/create")
     public ResponseEntity<String> createUser(@RequestBody User user) {
-        // Create a user and store it in the database
-        try {
-            List<User> users = userRepository.findAll();
-            boolean exists = users.stream().anyMatch(item -> item.getEmail().equals(user.getEmail()));
+        List<User> users = userRepository.findAll();
+        boolean exists = users.stream().anyMatch(item -> item.getEmail().equals(user.getEmail()));
 
-            // Check to see if a user with the current email is already registered
-            if (exists) {
-                return ResponseEntity.status(HttpStatus.CONFLICT).body("User already exists.");
-            } else {
-                userRepository.save(new User(user.getEmail(), user.encryptPassword()));
-                return ResponseEntity.status(HttpStatus.CREATED).body("A new user has been successfully created.");
-            }
+        // Check to see if a user with the current email is already registered
+        if (exists) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("User already exists.");
+        } else {
+            userRepository.save(new User(user.getEmail(), user.encryptPassword()));
+            return ResponseEntity.status(HttpStatus.CREATED).body("A new user has been successfully created.");
         }
-        catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to create new user.");
+    }
+
+    // Update a user's data in the database
+    @PutMapping("/users/update/{id}")
+    public ResponseEntity<String> updateUser(@PathVariable("id") Integer id, @RequestBody User user) {
+        Optional<User> currentData = userRepository.findById(id);
+        if (currentData.isPresent()) {
+            User newData = currentData.get();
+
+            // Only update values included in the request body
+            if (user.getEmail() != null) {
+                newData.setEmail(user.getEmail());
+            }
+            if (user.getPassword() != null) {
+                newData.setPassword(user.encryptPassword());
+            }
+
+            // Save the new changes
+            userRepository.save(newData);
+            return ResponseEntity.status(HttpStatus.OK).body("User with ID: " + id + " has been updated.");
+        } else {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("User with ID:" + id + " does not exist.");
+        }
+    }
+
+    // Delete an existing user from the database
+    @DeleteMapping("/users/delete/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable("id") Integer id) {
+        boolean exists = userRepository.existsById(id);
+        if (exists) {
+            userRepository.deleteById(id);
+            return ResponseEntity.status(HttpStatus.OK).body("User with ID: " + id + " has been deleted.");
+        } else {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("User with ID:" + id + " does not exist.");
         }
     }
 }
