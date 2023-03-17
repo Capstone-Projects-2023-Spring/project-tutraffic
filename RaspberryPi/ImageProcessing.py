d1f3a9a5e1949d1a8f5ee08e454c41a619fec9d0
Ethan Hopkins <tuk01901@temple.edu>
2023-03-17 15:11:58 -0400

    added methods to crop image and preprocess with grey and blur

--- RaspberryPi/ImageProcessing.py
+++ RaspberryPi/ImageProcessing.py
@@ -11,8 +11,23 @@ if __name__ == '__main__' :
  
     # Crop image
     imCrop = im[int(r[1]):int(r[1]+r[3]), int(r[0]):int(r[0]+r[2])]
+    # change colors to grey
     grey  = cv.cvtColor(imCrop, cv.COLOR_BGR2GRAY)
+    # gaussian blur the image
     blur = cv.GaussianBlur(grey,(5,5),0)
     # Display cropped image
     cv.imshow("Image", blur)
     cv.waitKey(0)
+
+def cropImage(Image):
+    if(Image.size !=0):
+        r = cv.selectROI(Image)
+        #crop image
+        cropped = im[int(r[1]):int(r[1]+r[3]), int(r[0]):int(r[0]+r[2])]
+        return cropped
+    
+def preprocess(Image):
+    if(Image.size !=0):
+        grey  = cv.cvtColor(imCrop, cv.COLOR_BGR2GRAY)
+        blur = cv.GaussianBlur(grey,(5,5),0)
+        return blur
