f016c9ec3f561a45e55bc816d3dde162e3feb776
Isaac Ferguson <tul24458@temple.edu>
2023-03-19 17:52:00 -0400

    cleaned up line detection code and added sending lines array to gcs bucket

--- RaspberryPi/detectLines.py
+++ RaspberryPi/detectLines.py
@@ -1,41 +1,47 @@
 import cv2
 import numpy as np
-import matplotlib
-from matplotlib.pyplot import imshow
-from matplotlib import pyplot as plt
-
-pics = ['RaspberryPi/2012-09-12_13_30_12.jpg',
-        'RaspberryPi/Cars-parked-in-parking-lot.jpeg',
-        'RaspberryPi/2013-02-22_06_05_00.jpg',
-        'RaspberryPi/plot.jpg',
-        'RaspberryPi/2013-03-01_17_53_00.jpg',
-        'RaspberryPi/parking.jpg']
-img = cv2.imread(pics[2])
-
-#blur and grey image
-greyImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-blurImg = cv2.GaussianBlur(greyImg,(5, 5),0)
-
-detectEdges = cv2.Canny(blurImg, 50, 150)
-
-rho = 1  # distance resolution in pixels of the Hough grid
-theta = np.pi / 240   # angular resolution in radians of the Hough grid
-threshold = 20  # minimum number of votes (intersections in Hough grid cell)
-min_line_length = 40  # minimum number of pixels making up a line
-max_line_gap = 5  # maximum gap in pixels between connectable line segments
-line_image = np.copy(img) * 0  # creating a blank to draw lines on
-
-# Run Hough on edge detected image
-# Output "lines" is an array containing endpoints of detected line segments
-lines = cv2.HoughLinesP(detectEdges, rho, theta, threshold, np.array([]),
-                    min_line_length, max_line_gap)
-
-for line in lines:
-    for x1,y1,x2,y2 in line:
-    	cv2.line(line_image,(x1,y1),(x2,y2),(0,0,255),5)
-    
-lines_edges = cv2.addWeighted(img, 0.8, line_image, 1, 0)
 
 
-cv2.imshow('final', lines_edges)
-cv2.waitKey(0)
+def detectLines(args):
+	pics = ['RaspberryPi/2012-09-12_13_30_12.jpg',
+			'RaspberryPi/Cars-parked-in-parking-lot.jpeg',
+			'RaspberryPi/2013-02-22_06_05_00.jpg',
+			'RaspberryPi/plot.jpg',
+			'RaspberryPi/2013-03-01_17_53_00.jpg',
+			'RaspberryPi/parking.jpg']
+	img = cv2.imread(pics[int(args)])
+
+
+	greyImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	blurImg = cv2.GaussianBlur(greyImg,(5, 5),0)
+
+	detectEdges = cv2.Canny(blurImg, 50, 200, None, 3)
+
+	rho = 1 
+	theta = np.pi / 240  
+	threshold = 15  # minimum number of votes (intersections in Hough grid cell)
+	min_line_length = 30  # minimum number of pixels making up a line
+	max_line_gap = 5  # maximum gap in pixels between connectable line segments
+	line_image = np.copy(img) * 0  
+
+
+	cdst = cv2.cvtColor(detectEdges, cv2.COLOR_GRAY2BGR)
+	cdstP = np.copy(cdst)
+
+	lines = cv2.HoughLinesP(detectEdges, rho, theta, threshold, np.array([]),
+						min_line_length, max_line_gap)
+
+	for line in lines:
+		for x1,y1,x2,y2 in line:
+			cv2.line(line_image,(x1,y1),(x2,y2),(0,0,255),5)
+		
+	lines_edges = cv2.addWeighted(img, 0.8, line_image, 1, 0)
+
+	# for testing to see lines overlayed on original image
+	#cv2.imshow('final', lines_edges)
+	#cv2.waitKey(0)
+
+	return lines
+
+if __name__ == '__main__':
+	detectLines()
---
+++ RaspberryPi/sendToServer.py
@@ -0,0 +1,44 @@
+import os
+from google.cloud import storage
+from detectLines import *
+import json
+from json import JSONEncoder
+import numpy as np
+import sys
+
+# permission to access GCS bucket
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'RaspberryPi/tutraffic1-92e09f8e1af7.json'
+storage_client = storage.Client()
+
+# send data # 
+def upload(blob_name, file_path, bucket_name):
+	try:
+		bucket = storage_client.get_bucket(bucket_name)
+		blob = bucket.blob(blob_name)
+		blob.upload_from_filename(file_path)
+		return True
+	except Exception as e:
+		print(e)
+		return False
+
+# class to convert Numpy ndarray into a list for JSON addition
+class NumpyArrayEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return JSONEncoder.default(self, obj)
+
+def convertToJson(array):
+	convertData = {"arrayOfParkingLines": array}
+	newJson = json.dumps(convertData, cls=NumpyArrayEncoder)
+	return newJson
+
+def main():
+	linesArray = detectLines(sys.argv[1])
+	toSend = convertToJson(linesArray)
+	print(toSend)
+	upload('ParkingLotLines',os.path.abspath(toSend),'parking-test-bucket')
+
+
+if __name__ == "__main__":
+		main()
