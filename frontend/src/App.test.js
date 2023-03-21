3383e62abf3ebe4552a080278bddc89bd9e90f63
Adam Wong <tewgon.734@gmail.com>
2023-03-21 02:29:23 +0000

    Add dummy test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,5 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { assert } from "console";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('dummy', () => {
+  expect(false).toBe(true);
 });
