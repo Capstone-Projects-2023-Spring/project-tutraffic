7e0b2aaf741b59fdd2c9452e65a0758c0e96125c
Brian Rangel <brianrang13@gmail.com>
2023-03-29 11:17:37 -0400

    Functionality for changing account password

--- frontend/src/components/AccountInfo.js
+++ frontend/src/components/AccountInfo.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { onAuthStateChanged } from 'firebase/auth';
+import { onAuthStateChanged, updatePassword } from 'firebase/auth';
 import { auth } from '../firebase';
 import { useNavigate } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
@@ -10,6 +10,9 @@ import './AccountInfo.css';
 const AccountInfo = () => {
   const navigate = useNavigate();
   const [user, setUser] = useState(null);
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [emailForm, setEmailForm] = useState(false);
   const [passwordForm, setPasswordForm] = useState(false);
   
   useEffect(() => {
@@ -31,6 +34,19 @@ const AccountInfo = () => {
     return <div>Please sign in to view your account information.</div>;
   }
 
+  const changePassword = async (e) => {
+    e.preventDefault();
+    const user = auth.currentUser;
+    updatePassword(user, newPassword).then(() => {
+      // Update successful.
+    }).catch((error) => {
+      // An error ocurred
+    });
+
+    setCurrentPassword("");
+    setNewPassword("");
+};
+
   return (
     <div className="settings">
       <h1>User Settings</h1>
@@ -47,32 +63,36 @@ const AccountInfo = () => {
             <Modal.Header closeButton>
               <Modal.Title>Update your password</Modal.Title>
             </Modal.Header>
+            <Form onSubmit={changePassword}>
               <Modal.Body>
-                <Form>
-                  <Form.Group controlId="current-password">
-                  <Form.Label>Current Password</Form.Label>
-                  <Form.Control
-                    type="password"
-                    name="currentPassword"
-                  />
-                  </Form.Group>
-                  <Form.Group controlId="new-password">
-                  <Form.Label>New Password</Form.Label>
-                  <Form.Control
-                    type="password"
-                    name="newPassword"
-                  />
-                  </Form.Group>
-                </Form>
+                <Form.Group controlId="current-password">
+                <Form.Label>Current Password</Form.Label>
+                <Form.Control
+                  value={currentPassword}
+                  onChange={(e) => setCurrentPassword(e.target.value)}
+                  type="password"
+                  name="currentPassword"
+                />
+                </Form.Group>
+                <Form.Group controlId="new-password">
+                <Form.Label>New Password</Form.Label>
+                <Form.Control
+                  value={newPassword}
+                  onChange={(e) => setNewPassword(e.target.value)}
+                  type="password"
+                  name="newPassword"
+                />
+                </Form.Group>
               </Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" type="reset" onClick={() => setPasswordForm(false)}>
-                Close
-              </Button>
-              <Button variant="primary" type="submit" onClick={() => setPasswordForm(false)}>
-                Save Changes
-              </Button>
-            </Modal.Footer>
+              <Modal.Footer>
+                <Button variant="secondary" type="reset" onClick={() => setPasswordForm(false)}>
+                  Close
+                </Button>
+                <Button variant="primary" type="submit" onClick={() => setPasswordForm(false)}>
+                  Save Changes
+                </Button>
+              </Modal.Footer>
+            </Form>
           </Modal>
         </div>
       </div>
