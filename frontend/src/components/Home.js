6c47d445985fa094154a38d37bb54d22e0073f1d
JM-CS <73761861+JM-CS@users.noreply.github.com>
2023-04-03 06:31:20 +0000

    Search bar that auto completes address and styling

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,18 +1,30 @@
 import React, { useState, useEffect } from 'react'
-import { GoogleMap, LoadScript, MarkerF } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, MarkerF, useLoadScript } from '@react-google-maps/api';
 import { useData } from './LotData';
 import blueDot from '../images/bluecircle.png';
+import usePlacesAutocomplete, {
+    getGeocode,
+    getLatLng,
+} from "use-places-autocomplete";
+import {
+    Combobox,
+    ComboboxInput,
+    ComboboxPopover,
+    ComboboxList,
+    ComboboxOption
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
+import styles from "./Home.css"
 
 export default function Home() {
-
     const containerStyle = {
         width: '100%',
         height: '95vh',
         position: 'relative',
     };
-
     const data = useData();
     const [center, setCenter] = useState({ lat: 39.981, lng: -75.155 });
+    const [selected, setSelected] = useState(null)
 
     useEffect(() => {
         if (navigator.geolocation) {
@@ -33,6 +45,52 @@ export default function Home() {
             console.error('Geolocation not supported by the browser.');
         }
     }, []);
+
+    const PlacesAutocomplete = ({ setSelected }) => {
+        const {
+            ready,
+            value,
+            setValue,
+            suggestions: {status, data},
+            clearSuggestions,
+        } = usePlacesAutocomplete({
+            requestOptions: {
+                types: ['address'],
+                componentRestrictions: {country: "us"}
+            },
+            debounce: 300,
+        });
+
+        const handleSelect = async (address) => {
+            setValue(address, false);
+            clearSuggestions();
+
+            try {
+                const results = await getGeocode({address});
+                const { lat, lng } = await getLatLng(results[0]);
+                setSelected({ lat, lng });
+                setCenter({ lat, lng });
+            } catch (error) {
+                console.log("Error: ", error);
+            }
+        };
+        return (
+            <Combobox onSelect={handleSelect}>
+                <ComboboxInput value={value} onChange={e => setValue(e.target.value)} disabled={!ready} className="combobox-input" placeholder="Search an Address"/>
+                <ComboboxPopover>
+                    <ComboboxList>
+                        {status === "OK" && data.map(({ id, description }) => <ComboboxOption key={id} value={description} />)}
+                    </ComboboxList>
+                </ComboboxPopover>
+            </Combobox>
+        );
+    };
+        //return <Combobox>
+        //    <ComboboxInput value={value} onChange={e => setValue(e.target.value)}  className="combobox-input" placeholder="Search an Address"/>
+        //</Combobox>
+        
+    //};
+
 
     const markers = data
         ? Object.keys(data).map((key) => {
@@ -57,10 +115,14 @@ export default function Home() {
 
 
 
-    return (
+   /* return (
         <LoadScript
             googleMapsApiKey={process.env.REACT_APP_GOOGLEMAP_API_KEY}
+            libraries={["places"]}
         >
+            <div className="places-container">
+                <PlacesAutocomplete setSelected={setSelected} />
+            </div>
             <GoogleMap
                 mapContainerStyle={containerStyle}
                 center={center}
@@ -81,8 +143,30 @@ export default function Home() {
                         options={marker.options}
                     />
                 ))}
+                {selected && <MarkerF position={selected} />}
             </GoogleMap>
         </LoadScript>
     );
-
+*/
+return (
+    <LoadScript
+    googleMapsApiKey={process.env.REACT_APP_GOOGLEMAP_API_KEY}
+    libraries={["places"]}
+    >
+        <div className="places-container">
+            <PlacesAutocomplete setSelected={setSelected} />
+        </div>
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={16}>
+            <MarkerF position={center} options={{ icon: {url: blueDot,},}}/>
+            {markers.map((marker, index) => (
+            <MarkerF
+                    key={index}
+                    position={marker.position}
+                    options={marker.options}
+                />
+            ))}
+            {selected && <MarkerF position={selected} />}
+        </GoogleMap>
+    </LoadScript>
+    );
 }
