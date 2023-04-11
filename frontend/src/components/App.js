8a3f6254c91025c909f1187642327a98ddc9a79e
Brian Rangel <61568328+brian-rangel@users.noreply.github.com>
2023-04-11 15:18:31 +0000

    Fixed contents of the map overflowing on mobile webpage

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { useLoadScript } from '@react-google-maps/api';
 
@@ -8,24 +8,39 @@ import AppRoutes from "./Routes";
 const libraries = ['places'];
 
 export const App = () => {
+  const [windowDimension, setWindowDimension] = useState(null);
 
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLEMAP_API_KEY,
     libraries,
   });
 
+  useEffect(() => {
+    setWindowDimension(window.innerWidth);
+  }, []);
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimension(window.innerWidth);
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const isMobile = windowDimension <= 640;
+
   return (
     <>
       <Router>
-        <div className="App" data-testid="app-component" style={{ height: "100vh", display: "flex", flexDirection: "column", fontFamily: "Arial, Helvetica, sans-serif"}}>
-          <div>
-            <Navigation />
-          </div>
+        <div className="App" data-testid="app-component" style={{ height: "100vh", height: "100dvh", display: "flex", flexDirection: "column", fontFamily: "Arial, Helvetica, sans-serif"}}>
+          {!isMobile && <Navigation />}
           <div style={{ flexGrow: "1" }}>
             {loadError && <div>Error loading google maps API</div>}
             {!isLoaded && !loadError && <div>Loading google maps API</div>}
             {isLoaded && <AppRoutes />}
           </div>
+          {isMobile && <Navigation />}
         </div>
       </Router>
     </>
