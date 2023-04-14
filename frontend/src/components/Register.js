bd030f13cad956a1c0686f2665ce9289fc55b929
Brian Rangel <61568328+brian-rangel@users.noreply.github.com>
2023-04-14 19:15:59 +0000

    Reimplemented register page

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,59 +1,115 @@
 import React, { useState } from 'react';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
 import { useNavigate } from "react-router-dom";
-
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import './LoginRegister.css';
 
 export const Register = (props) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const navigate = useNavigate();
-    const [showErrorModal, setShowErrorModal] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [textColor, setTextColor] = useState(0);
+  const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-            console.log("Success:", userCredential);
-            navigate('/');
-        } catch (error) {
-            console.error('Error registering user with email and password:', error);
-            setShowErrorModal(true);
-            setTimeout(() => {
-                setShowErrorModal(false);
-            }, 2000);
-        }
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setTextColor(0);
 
-    const onFormSwitch = () => {
-        navigate('/account/login')
+    // These are called before await in order to display the correct message
+    if (email === "") {
+      setMessage("Please enter a valid email address.");
+      return;
     }
-    return (
-        <div className='auth-form-container'>
-            <div>
-                <h1>Register</h1>
-                <form className='register-form' onSubmit={handleSubmit}>
-                    <input value={email} onChange={(e) => setEmail(e.target.value)} type='email' placeholder="Email@gmail.com" id='email' name='email' />
-                    <input value={password} onChange={(e) => setPassword(e.target.value)} type='password' placeholder="Password" id='password' name='password' />
-                    <Button variant="primary" type="submit">Register</Button>{''}
-                </form>
-                <Modal show={showErrorModal} onHide={() => setShowErrorModal(false)}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Registration Error</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>There was an error registering your account. Please try again.</Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="secondary" onClick={() => setShowErrorModal(false)}>
-                            Close
-                        </Button>
-                    </Modal.Footer>
-                </Modal>
-                <Button className='formswitch' onClick={onFormSwitch} variant="outline-secondary" size="sm">Already have an account?</Button>{' '}
+    if (password === "") {
+      setMessage("Please enter a password.");
+      return;
+    }
+    if (confirmPassword === "") {
+      setMessage("Please confirm your password.");
+      return;
+    }
+    if (confirmPassword !== password) {
+      setMessage("Passwords do not match.");
+      return;
+    }
+
+    await createUserWithEmailAndPassword(auth, email, password).then(() => {
+      console.log("Successful login");
+      setTextColor(1);
+      setMessage("Account successfully created. You will soon be redirected.");
+      setTimeout(() => {
+        navigate('/');
+      }, 2000);
+    }).catch((error) => { 
+      console.log(error.message);
+      switch (error.code) {
+        case 'auth/email-already-in-use':
+          setMessage("Email already in use by another user.");
+          break;
+        case 'auth/weak-password':
+          setMessage("Password must be at least 6 characters long.");
+          break;
+        default:
+          setMessage("Error registering user.");
+      }
+    });
+  };
+
+  const onFormSwitch = () => {
+    navigate('/account/login')
+  }
+
+  return (
+    <div className="auth-form-container">
+      <Card style={{ width: '30rem' }}>
+        <Card.Body>
+          <Card.Title className="text-center mt-3">Create Your Account</Card.Title>
+          <p className="center-item">Sign up for TuTraffic</p>
+          <p style={{color: textColor ? "blue" : "red" }}>{message}</p> 
+          <Form className="login-form" onSubmit={handleSubmit}>
+            <Form.Group controlId="email">
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                name="email"
+              />
+            </Form.Group>
+            <Form.Group controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+                name="password"
+              />
+            </Form.Group>
+            <Form.Group controlId="confirm-password">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                type="password"
+                name="confirmPassword"
+              />
+            </Form.Group>
+            <div className="login-btn mt-4">
+              <Button variant="primary" type="submit">Create</Button>
             </div>
-        </div>
-    );
+          </Form>
+          <div className="center-item mt-4">
+            <p>Already have an account?</p>
+            <p style={{fontWeight:"bold", cursor:"pointer", color:"#4170cc"}} onClick={onFormSwitch}>Log in</p>
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
 
 export default Register;
