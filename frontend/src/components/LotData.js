3f91698a484a4e1811b6cc41a35ff2932d339ee9
Guire9 <60325869+Guire9@users.noreply.github.com>
2023-04-16 18:24:55 +0000

    Allow user to filter parking by type

--- frontend/src/components/LotData.js
+++ frontend/src/components/LotData.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
-import { database } from '../firebase';
+import { database, auth, db } from '../firebase';
 import { ref, get } from 'firebase/database';
+import { onAuthStateChanged } from 'firebase/auth';
+import { collection, doc, getDoc } from 'firebase/firestore';
 
 export const LotData = () => {
   const [data, setData] = useState(null);
@@ -9,7 +11,34 @@ export const LotData = () => {
     const fetchData = async () => {
       const parkingRef = ref(database, 'parking');
       const snapshot = await get(parkingRef);
-      setData(snapshot.val());
+      const parkingObjects = snapshot.val();
+      const streetParking = [];
+      const lotParking = [];
+
+      onAuthStateChanged(auth, async (user) => {
+        if (user) {
+          const usersRef = doc(collection(db, 'users'), user.uid);
+          const docSnap = await getDoc(usersRef);
+
+          if (docSnap.exists() && docSnap.data().lotType === 'street') {
+            for (const value of Object.values(parkingObjects)) {
+              if (value.street) {
+                streetParking.push(value);
+              }
+            }
+            setData(streetParking);
+          } else if (docSnap.exists() && docSnap.data().lotType === 'lot'){
+            for (const value of Object.values(parkingObjects)) {
+              if (!value.street) {
+                lotParking.push(value);
+              }
+            }
+            setData(lotParking);
+          }
+        } else {
+          setData(parkingObjects);
+        }
+      });
     };
 
     fetchData();
