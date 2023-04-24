50ec5fe54d9ebfbc1541f059a8c06f18defc8838
Adam Wong <tue91717@temple.edu>
2023-04-23 21:11:05 -0400

    Revert "Add fake key for testing loading."
    This reverts commit 6158e28b016ec996b2c588fced56edc29eac362b.

--- frontend/cypress/python/mocklot.py
+++ frontend/cypress/python/mocklot.py
@@ -57,9 +57,8 @@ LOT_PROP = {
 }
 # Firebase Authentication.
 try:
-  file = open('fake-key.json')
+  file = open('tutraffic-firebase-key.json')
   data = load(file)
-  print(data)
   cred = credentials.Certificate(data)
 except Exception as e:
   print(e)
