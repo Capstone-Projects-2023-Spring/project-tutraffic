a84dd32fed7f002131812c0baa5fbc7b9e1a7002
Guire9 <60325869+Guire9@users.noreply.github.com>
2023-04-24 18:04:59 +0000

    merger should now work

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -97,7 +97,7 @@ const Map = () => {
   const markers = data
   ? Object.keys(data)
   .filter((key) => (userLotType !== null ? data[key].street === userLotType : true)) //added filter
-  .filter((key) => (userCarType !== null ? data[key].maxsize=== userCarType : true)) //added filter
+  .filter((key) => (userCarType !== null ? data[key].maxsize >= userCarType : true)) //added filter
     .map((key) => {
         const { lat, lng, spots, street } = data[key];
         if (lat && lng) {
