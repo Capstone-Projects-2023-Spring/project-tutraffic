b4099be5e65bab5316555032ab8061453fa5d329
Brian Rangel <61568328+brian-rangel@users.noreply.github.com>
2023-04-28 22:55:45 +0000

    Completely restyled the user profile page

--- frontend/src/components/AccountProfile.js
+++ frontend/src/components/AccountProfile.js
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth, db } from '../firebase';
 import { setDoc, doc, getDoc } from 'firebase/firestore';
+import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import './UserSettings.css';
 
@@ -85,28 +86,42 @@ const AccountProfile = () => {
             <Nav.Link className="highlighted-btn" onClick={() => navigate("/account/profile")}>User Profile</Nav.Link>
           </Nav.Item>
         </Nav>
+
+        <Container style={{display:"flex", justifyContent:"center", alignContent:"center"}}>
+          <Container style={{ width: "800px", }}>
+            <Container className="user-banner">
+              <div className="icon-border">
+                <img src={`${process.env.PUBLIC_URL}/images/defaulticon.png`} className="user-icon"></img>
+              </div>
+            </Container>
+            <Container className="profile-settings">
+              <div className="user-opts">
+                <div style={{color: "rgb(100,100,100)"}}>CURRENT CAR SIZE: {carSize}</div>
+              </div>
+              <select className="dropdown-btns" value={carSize} onChange={(e) => setCarSize(e.target.value)}>
+                <option value="Compact+">All Parking</option>
+                <option value="Average">Exclude Compact Spaces</option>
+                <option value="Large">Truck or Large Vehicle</option>
+              </select>
+              <div className="user-opts">
+                <div style={{color: "rgb(100,100,100)"}}>PREFFERED LOT TYPE: {lotType}</div>
+              </div>
+              <select className="dropdown-btns" value={lotType} onChange={(e) => setLotType(e.target.value)}>
+                <option value="both">All Parking</option>
+                <option value="lot">Lot or Garage</option>
+                <option value="street">Street Parking</option>
+              </select>
+              <div className="user-opts">
+                <div style={{color: "rgb(100,100,100)"}}>PRICING OPTION: {priceType}</div>
+              </div>
+              <select className="dropdown-btns" value={priceType} onChange={(e) => setPriceType(e.target.value)}>
+              <option value="all">All Parking</option>
+                <option value="free">Only Free Parking</option>
+              </select>
+            </Container>
+          </Container>
+        </Container>
       </div>
-      <div className="profile">
-        <h2>User Profile</h2>
-        <h4>Current Car Size: {carSize}</h4>
-        <select value={carSize} onChange={(e) => setCarSize(e.target.value)}>
-          <option value="Compact+">All Parking</option>
-          <option value="Average">Exclude Compact Spaces</option>
-          <option value="Large">Truck or Large Vehicle</option>
-        </select>
-        <h4>Current Lot Type: {lotType}</h4>
-        <select value={lotType} onChange={(e) => setLotType(e.target.value)}>
-          <option value="both">All Parking</option>
-          <option value="lot">Lot or Garage</option>
-          <option value="street">Street Parking</option>
-        </select>
-        <h4>Current price Type: {priceType}</h4>
-        <select value={priceType} onChange={(e) => setPriceType(e.target.value)}>
-        <option value="all">All Parking</option>
-          <option value="free">Only Free Parking</option>
-        </select>
-        
-        </div>
     </div>
   );
 };
