1311b1322c71d8288039037c73b1c0f1df431872
Adam Wong <tue91717@temple.edu>
2023-04-29 08:59:54 -0400

    Group tests under one block.

--- testing/cypress/e2e/find_parking_nearest_with_filters.spec.cy.js
+++ testing/cypress/e2e/find_parking_nearest_with_filters.spec.cy.js
@@ -1,5 +1,10 @@
 /**
- * Test the use cases for find parking with user-defined filters.
+ * Test the use case to find parking spaces based on user preferences.
+ * Expected Result: The user can filter by free parking, or by between garage or street parking.
+ * Test Components:
+ *   Firebase - Test read-write access to the Firebase 'account/' and 'parking/' document.
+ *   Frontend - Test navigation within the following web app endpoints: root,'/account/login',
+ *   '/account/info', 'account/profile', '/browse', '/map', and '/parkinglot/{key}'.
  */
 import { createAccount, deleteAccount } from '../support/firebase';
 
@@ -18,23 +23,23 @@ Cypress.Commands.add("queryMapThenBrowse", () => {
     .should('eq', '/browse')
 })
 
-  /**
-   * Change the user lot type and price type settings.
-   * Parameter:
-   * lotType - The new value for the lot type setting.
-   * priceType - The new value for the price type setting.
-   */
-  Cypress.Commands.add("updateSetting", (lotType, priceType) => {
-    cy.get(':nth-child(2) > .nav-link').click()
-    cy.location('pathname')
-      .should('eq', '/account/profile')
-    // Set lot type using drop-down selection.
-    cy.get('.profile-settings > :nth-child(4)')
-      .select(lotType)
-    // Set price type using drop-down selection.
-    cy.get('.profile-settings > :nth-child(6)')
-      .select(priceType)
-  })
+/**
+ * Change the user lot type and price type settings.
+ * Parameter:
+ * lotType - The new value for the lot type setting.
+ * priceType - The new value for the price type setting.
+ */
+Cypress.Commands.add("updateSetting", (lotType, priceType) => {
+  cy.get(':nth-child(2) > .nav-link').click()
+  cy.location('pathname')
+    .should('eq', '/account/profile')
+  // Set lot type using drop-down selection.
+  cy.get('.profile-settings > :nth-child(4)')
+    .select(lotType)
+  // Set price type using drop-down selection.
+  cy.get('.profile-settings > :nth-child(6)')
+    .select(priceType)
+})
 
 before(() => {
   createAccount()
@@ -52,98 +57,62 @@ after(() => {
   cy.exec('python cypress/python/mocklot.py delete mockfreest')
 })
 
-/**
- * Test the use case to find parking lots or street parking exclusively.
- * Expected Result: The user can view parking lots or street parking exclusively.
- * Test Components:
- *   Firebase - Test read-write access to the Firebase 'account/' and 'parking/' document.
- *   Frontend - Test navigation within the following web app endpoints: root,'/account/login',
- *   '/account/info', 'account/profile', '/browse', '/map', and '/parkinglot/{key}'.
- */
-describe('Lot Type Filter', () => {
-  it('Logs in to the user, sets lot type to street, then finds only street parking.', () => {
-    cy.userLogin()
-
-    cy.updateSetting('Street Parking', 'All Parking')
-    cy.queryMapThenBrowse()
-
-    // Verify that only street parking exists.
-    cy.contains('mockpaidlot').should('not.exist')
-    cy.contains('mockpaidst')
-    cy.contains('mockfreelot').should('not.exist')
-    cy.contains('mockfreest')
-  })
-
-  it('Logs in to the user, sets lot type to lot, then finds only parking lots.', () => {
-    cy.userLogin()
-
-    cy.updateSetting('Lot or Garage', 'All Parking')
-    cy.queryMapThenBrowse()
-
-    // Verify that only lot parking exists.
-    cy.contains('mockpaidlot')
-    cy.contains('mockpaidst').should('not.exist')
-    cy.contains('mockfreelot')
-    cy.contains('mockfreest').should('not.exist')
-  })
+beforeEach(() => {
+  cy.userLogin()
+  cy.visit('https://tutrafficdatabase.web.app/account/info')
 })
 
-/**
- * Test the use case to find free parking spaces.
- * Expected Result: The user can view free parking spaces exclusively.
- * Test Components:
- *   Firebase - Test read-write access to the Firebase 'account/' and 'parking/' document.
- *   Frontend - Test navigation within the following web app endpoints: root,'/account/login',
- *   '/account/info', 'account/profile', '/browse', and '/map'.
- */
-describe('Free Parking Filter', () => {
-  it('Logs in to the user, sets price to free, then finds free parking.', () => {
-    cy.userLogin()
+it('Logs in to the user, sets lot type to street, then finds only street parking.', () => {
+  cy.updateSetting('Street Parking', 'All Parking')
+  cy.queryMapThenBrowse()
 
-    cy.updateSetting('All Parking', 'Only Free Parking')
-    cy.queryMapThenBrowse()
-
-    // Verify that only free parking exists.
-    cy.contains('mockpaidlot').should('not.exist')
-    cy.contains('mockpaidst').should('not.exist')
-    cy.contains('mockfreelot')
-    cy.contains('mockfreest') // This specific assert sometimes fail.
-  })
+  // Verify that only street parking exists.
+  cy.contains('mockpaidlot').should('not.exist')
+  cy.contains('mockpaidst')
+  cy.contains('mockfreelot').should('not.exist')
+  cy.contains('mockfreest')
 })
 
-/**
- * Test the composition of applied filters.
- * Expected Result: The user can view combine the free parking and street parking filters to view
- * free street parking or free parking lots.
- * Test Components:
- *   Firebase - Test read-write access to the Firebase 'account/' and 'parking/' document.
- *   Frontend - Test navigation within the following web app endpoints: root,'/account/login',
- *   '/account/info', 'account/profile', '/browse', and '/map'.
- */
-describe('Composition of applied filters', () => {
-  beforeEach(() => {
-    cy.userLogin()
-  })
+it('Logs in to the user, sets lot type to lot, then finds only parking lots.', () => {
+  cy.updateSetting('Lot or Garage', 'All Parking')
+  cy.queryMapThenBrowse()
 
-  it('Logs in to the user, sets filters to free parking lots, and finds a free parking lot.', () => {
-    cy.updateSetting('Lot or Garage', 'Only Free Parking')
-    cy.queryMapThenBrowse()
+  // Verify that only lot parking exists.
+  cy.contains('mockpaidlot')
+  cy.contains('mockpaidst').should('not.exist')
+  cy.contains('mockfreelot')
+  cy.contains('mockfreest').should('not.exist')
+})
 
-    // Verify that only free parking lots exists.
-    cy.contains('mockpaidlot').should('not.exist')
-    cy.contains('mockpaidst').should('not.exist')
-    cy.contains('mockfreelot')
-    cy.contains('mockfreest').should('not.exist')
-  })
+it('Logs in to the user, sets price to free, then finds free parking.', () => {
+  cy.updateSetting('All Parking', 'Only Free Parking')
+  cy.queryMapThenBrowse()
 
-  it('Logs in to the user, sets filters to free street parking, and finds free street parking.', () => {
-    cy.updateSetting('Street Parking', 'Only Free Parking')
-    cy.queryMapThenBrowse()
+  // Verify that only free parking exists.
+  cy.contains('mockpaidlot').should('not.exist')
+  cy.contains('mockpaidst').should('not.exist')
+  cy.contains('mockfreelot')
+  cy.contains('mockfreest')
+})
 
-    // Verify that only free street parking exists.
-    cy.contains('mockpaidlot').should('not.exist')
-    cy.contains('mockpaidst').should('not.exist')
-    cy.contains('mockfreelot').should('not.exist')
-    cy.contains('mockfreest')
-  })
+it('Logs in to the user, sets filters to free parking lots, then finds a free parking lot.', () => {
+  cy.updateSetting('Lot or Garage', 'Only Free Parking')
+  cy.queryMapThenBrowse()
+
+  // Verify that only free parking lots exists.
+  cy.contains('mockpaidlot').should('not.exist')
+  cy.contains('mockpaidst').should('not.exist')
+  cy.contains('mockfreelot')
+  cy.contains('mockfreest').should('not.exist')
+})
+
+it('Logs in to the user, sets filters to free street parking, and finds free street parking.', () => {
+  cy.updateSetting('Street Parking', 'Only Free Parking')
+  cy.queryMapThenBrowse()
+
+  // Verify that only free street parking exists.
+  cy.contains('mockpaidlot').should('not.exist')
+  cy.contains('mockpaidst').should('not.exist')
+  cy.contains('mockfreelot').should('not.exist')
+  cy.contains('mockfreest')
 })
